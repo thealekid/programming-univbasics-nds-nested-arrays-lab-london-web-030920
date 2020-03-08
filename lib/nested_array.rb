88394c5d377e6239c15dc8adf1db88b09dcc6763
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-08 20:38:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,7 +1,7 @@
 # Examples inspired by U. S. National Organic Standards
 
-ORGANIC_PRODUCE = ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
 CONVENTIONAL_PRODUCE = ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"]
+ORGANIC_PRODUCE = ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
 
 def assembled_matrix 
   
